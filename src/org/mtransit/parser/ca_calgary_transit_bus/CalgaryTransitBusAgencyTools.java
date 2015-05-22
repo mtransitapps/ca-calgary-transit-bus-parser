9f7b4e050ba1b45070252b997d0523521823ba40
Mathieu Méa <mathieu.mea@gmail.com>
2015-05-22 08:33:38 -0400

    Compatibility with shared parser library & code cleaning.

--- src/org/mtransit/parser/ca_calgary_transit_bus/CalgaryTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_calgary_transit_bus/CalgaryTransitBusAgencyTools.java
@@ -10,6 +10,7 @@ import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MAgency;
@@ -551,8 +552,7 @@ public class CalgaryTransitBusAgencyTools extends DefaultAgencyTools {
 	private static final String WOODLANDS = "Woodlands";
 
 	@Override
-	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip) {
-
+	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
 		if (mRoute.id == 1l) {
 			if (gTrip.direction_id == 0) {
 				mTrip.setHeadsignString(FOREST_LAWN, gTrip.direction_id);
