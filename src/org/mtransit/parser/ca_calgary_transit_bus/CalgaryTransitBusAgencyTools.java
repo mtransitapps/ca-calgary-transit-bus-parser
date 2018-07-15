ad6eefe966c56a052f981de6815971b2a5f1820c
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-15 01:51:28 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_calgary_transit_bus/CalgaryTransitBusAgencyTools.java
+++ src/org/mtransit/parser/ca_calgary_transit_bus/CalgaryTransitBusAgencyTools.java
@@ -68,6 +68,11 @@ public class CalgaryTransitBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
